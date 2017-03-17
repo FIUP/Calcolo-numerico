fdaa50041add12c3bce0b912107fb080d41352ac
Stefano Fogarollo <sirfoga@protonmail.com>
2017-03-17 10:42:18 +0100

    added 3-ch algs

---
+++ mrz-3-edition/chapters/3/exercises/3/squaredMultiplications.m
@@ -0,0 +1,51 @@
+%{
+ % Copyright 2017 Stefano Fogarollo
+ %
+ % Licensed under the Apache License, Version 2.0 (the "License");
+ % you may not use this file except in compliance with the License.
+ % You may obtain a copy of the License at
+ %
+ % http://www.apache.org/licenses/LICENSE-2.0
+ %
+ % Unless required by applicable law or agreed to in writing, software
+ % distributed under the License is distributed on an "AS IS" BASIS,
+ % WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ % See the License for the specific language governing permissions and
+ % limitations under the License.
+%}
+
+
+function [o] = squaredMultiplications (n)
+% SQUAREDMULTIPLICATIONS Evaluates f(n) for n. f(n) is defined to be
+% (sqrt((n) / (n - 1) * (n - 1) / (n - 2) * ... * 1 / 1)) ^ 2 *
+%  (n - 1) / (n) * (n - 2) / (n - 1) * ... * 1 / 1
+%
+% [o] = squaredMultiplications (n)
+%
+% Input:
+% n - the integer to copmute the function f(n)
+%
+% Output:
+% o - f(n) computed with no smart approximations
+
+%% First part:
+% o = (Prod[i = 0 to n - 2] (n - i) / (n - i - 1) ) * (n - (n - 1)) / 1
+
+o = (n - (n - 1)) / 1.0;
+for i = 0 : n - 2
+    o = o * (n - i) / (n - i - 1);
+end
+
+%% Second part:
+% o = sqrt(o) ^ 2
+
+o = sqrt(o);
+o = o ^ 2;
+
+%% Third part:
+% o = (Prod[i = 0 to n - 2] (n - i - 1) / (n - i) ) * 1 / (n - (n - 1))
+
+o = o * 1 / (n - (n - 1));
+for i = 0 : n - 2
+    o = o * (n - i - 1) / (n - i);
+end
---
+++ mrz-3-edition/chapters/3/exercises/6/parabolaSolverComplexVersion.m
@@ -0,0 +1,56 @@
+%{
+ % Copyright 2017 Stefano Fogarollo
+ %
+ % Licensed under the Apache License, Version 2.0 (the "License");
+ % you may not use this file except in compliance with the License.
+ % You may obtain a copy of the License at
+ %
+ % http://www.apache.org/licenses/LICENSE-2.0
+ %
+ % Unless required by applicable law or agreed to in writing, software
+ % distributed under the License is distributed on an "AS IS" BASIS,
+ % WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ % See the License for the specific language governing permissions and
+ % limitations under the License.
+%}
+
+
+function [x1, x2] = parabolaSolverComplexVersion (a, b, c)
+% PARABOLASOLVERCOMPLEXVERSION Solves parabola equation also in C
+%
+% [x1, x2] = parabolaSolverComplexVersion (a, b, c)
+%
+% Given a parabola equation like ax² + bx + c = 0
+%
+% Input:
+% a - coefficient of x²
+% b - coefficient of x
+% c - known term
+%
+% Output:
+% x1 - first solution
+% x2 - second solution
+
+if a == 0
+    if b == 0
+        if c == 0
+            x1 = NaN;  % 0 + 0 + 0 = 0 ... not determined
+            x2 = NaN;
+        else
+            x1 = NaN;  % 0 + 0 + c = 0 ... impossible!
+            x2 = NaN;
+        end
+    else
+        x1 = -c / b;  % bx + c = 0 ... 1-deg equation
+        x2 = x1;
+    end
+else
+    delta = b ^ 2 - 4 * a * c;  % calculate D
+    if delta == 0
+        x1 = -b / (2 * a);  % 2 equals solutions
+        x2 = x1;
+    else
+        x1 = (-b - sign(b) * sqrt(delta)) / (2 * a);  % standard 2-deg equation
+        x2 = c / (a * x1);  % clever way to reduce numerical errors
+    end
+end
