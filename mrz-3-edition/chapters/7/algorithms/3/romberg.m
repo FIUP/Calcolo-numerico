67678ff7f5333fbf00b1df94aaa37226e73e7171
Stefano Fogarollo <sirfoga@protonmail.com>
2017-05-28 20:22:44 +0200

    added romberg integration method

--- mrz-3-edition/chapters/7/algorithms/3/romberg.m
+++ mrz-3-edition/chapters/7/algorithms/3/romberg.m
@@ -1,21 +1,22 @@
-function [x, numberOfIterations] = multipleRootsNewton(f, fDerivative, startPoint, tolerance, maxIterations, r)
-% MULTIPLEROOTSNEWTON: Finds a solution of f(x) = 0 with Newton method.
+function [I] = romberg(f, a, b, m, n)
+% SIMPSON: Finds the value of integral(f) from a to b using the
+% Romberg rule with m intervals.
 %
-%  [x, numberOfIterations, derivativeZero] = multipleRootsNewton(f, fDerivative, startPoint, tolerance, maxIterations, r)
+%  [I] = romberg(f, a, b, m, n)
 %
 % Input:
-% f - 'f' function in the equation 'f(x) = 0'
-% fDerivative - derivative of f
-% startPoint - starting point of method
-% tolerance - epsilon at which stop method (i.e when |f(xn) - 0| <
-%             epsilon)
-% maxIterations - max number of iterations to execute
-% r - multiplicity of the solution
+% f - function to integrate
+% a - start point
+% b - end point
+% m - number of intervals to create
+% n - number of initial values of the first column of integral values
 %
 % Output:
-% x - approximation of solution of 'f(x) = 0'
-% numberOfIterations - number of iterations executed before getting
-%                      solution
+% I - approximation of solution of 'f(x) = x'
+% numberOfIterations - number of integral(f) from a to b
+% NEWTON: Finds a solution of f(x) = 0 with Newton method.
+%
+%  [x, numberOfIterations, derivativeZero] = newton(f, fDerivative, startPoint, tolerance, maxIterations)
 
 % Copyright 2017 Stefano Fogarollo
 %
@@ -25,21 +26,19 @@ function [x, numberOfIterations] = multipleRootsNewton(f, fDerivative, startPoin
 %
 % http://www.apache.org/licenses/LICENSE-2.0
 %
-% Unless required by applicable law or agreed to in writing, software
+% Unless require4d by applicable law or agreed to in writing, software
 % distributed under the License is distributed on an "AS IS" BASIS,
 % WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 % See the License for the specific language governing permissions and
 % limitations under the License.
 
-numberOfIterations = 0;
-x = startPoint;
-deltaDiff = tolerance * 2;  % initialize diff
-while deltaDiff >= tolerance && numberOfIterations < maxIterations
-    derivativeValue = feval(fDerivative, x);
-    if derivativeValue ~= 0
-        deltaDiff = - r * feval(f, x) / feval(fDerivative, x);
-        x = x + deltaDiff;
-        deltaDiff = abs(deltaDiff);
+T = zeros(n, n);  % initialize table
+for i = 1 : n  % fill first column
+    T(i, 1) = trapezoidalRule(f, a, b, m);
+    m = 2 * m;
+end
+for k = 1 : n - 1
+    for i = 0 : n - k - 1
+        T(i, k + 1) = (4 ^ (k + 1) * T(i + 1, k) - T(i, k)) / (4 ^ (k + 1) - 1);
     end
-    numberOfIterations  = numberOfIterations + 1;  % increase counter
 end
